48c606f3588afccd64b148c3352190e5fa8d58c6
sidney zhang <sidazhang89@gmail.com>
2013-06-01 13:43:34 -0700

    init comit with rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RezzoRails::Application.config.secret_token = '6d1a6471a943317f986bf479d11c752cd29321157541a41e2ac879ff61cef59a4b15fa60a73c8ac3a8d22bdbf096a23d30c1e020e8c71267f79d3ffea247b9bd'
