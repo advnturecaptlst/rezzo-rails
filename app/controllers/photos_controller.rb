ad8fb1f7d2db054a0f04b3c842478684e91050c5
Steven Miyakawa SAM <samprofessional@gmail.com>
2013-06-01 17:45:13 -0700

    Install fusion table gem and code to test fusion table

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -3,7 +3,18 @@ class PhotosController < ApplicationController
   end
 
   def create
-    p params
-    debugger
+  end
+
+  private
+
+  #just to test fusion table
+  def save_to_fusion_table
+    ft = GData::Client::FusionTables.new 
+    ft.clientlogin(ENV['GOOGLE_USERNAME'], ENV['GOOGLE_PASS'])
+    ft.set_api_key(ENV['GOOGLE_KEY']) # obtained from the google api console
+    # Creating a table
+    cols = [{:name => "Sam Samskies", :type => 'string' }]
+
+    ft.create_table "Testing2", cols
   end
 end
