cd82f88056c25ba2f077bfc6c7603a336f79adb2
Steven Miyakawa SAM <samprofessional@gmail.com>
2013-06-16 15:40:07 -0700

    Change route for incoming request from iOS app.

--- rezzo.rb
+++ rezzo.rb
@@ -10,7 +10,7 @@ get "/" do
  haml :new
 end
 
-post "/" do
+post "/ios" do
   params.each do |k, v|
     data = JSON.parse(v)
     latitude = data['latitude']
@@ -24,11 +24,11 @@ post "/android" do
   # uploaded_io = params["picture"]
   # @filename = params[:picture][:filename]
   # file = params[:picture][:tempfile]
- 
+
   # File.open("./public/#{@filename}", 'wb') do |f|
   #   f.write(file.read)
   # end
-  
+
   # photo_metadata = EXIFR::JPEG.new("./public/" + @filename)
   # latitude = photo_metadata.gps.latitude
   # longitude = photo_metadata.gps.longitude
@@ -40,9 +40,9 @@ private
   def save_to_fusion_table(latitude, longitude)
     p latitude
     p longitude
-    ft = GData::Client::FusionTables.new 
+    ft = GData::Client::FusionTables.new
     ft.clientlogin(ENV['GOOGLE_USERNAME'], ENV['GOOGLE_PASS'])
-    ft.set_api_key(ENV['GOOGLE_KEY']) 
+    ft.set_api_key(ENV['GOOGLE_KEY'])
 
     tables = ft.show_tables
     rezzo  = tables.select{|t| t.name == "Togo"}.first
